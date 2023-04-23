c4d4f113af49a2a2a6e95633ff090f9816a50636
Ni Qiang <ni.qiang@imt-atlantique.net>
2023-04-23 20:24:40 +0200

    euclidean gcd

--- epi_judge_cpp/is_tree_balanced.cc
+++ epi_judge_cpp/is_tree_balanced.cc
@@ -1,8 +1,29 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
+#include <unordered_map>
+#include <utility>
+
+void getHeight(const unique_ptr<BinaryTreeNode<int>>& tree, std::unordered_map<unique_ptr<BinaryTreeNode<int>>&, int>& heightResult) {
+  if (tree != nullptr) {
+    if (tree->left == nullptr && tree->right == nullptr) {
+      heightResult.emplace(std::move(tree), 1);
+    } else {
+      getHeight(tree->left, heightResult);
+      getHeight(tree->right, heightResult);
+      heightResult.emplace(std::move(tree), std::max(heightResult[tree->left], heightResult[tree->right]) + 1);
+    }
+  } 
+}
 bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& tree) {
   // TODO - you fill in here.
-  return true;
+  std::unordered_map<unique_ptr<BinaryTreeNode<int>>&, int> heightMap;
+  if (tree) {
+    getHeight(tree->left, heightMap);
+    getHeight(tree->right, heightMap);
+    heightMap.emplace(std::move(tree),std::max(heightMap[tree->left], heightMap[tree->right]) + 1);
+  }
+
+
 }
 
 int main(int argc, char* argv[]) {
