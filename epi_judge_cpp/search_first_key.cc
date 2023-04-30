06a6f92e5a8edb863346b4ed809ddca85f493e40
Ni Qiang <ni.qiang@imt-atlantique.net>
2023-04-30 17:18:38 +0200

    search_first_key

--- epi_judge_cpp/search_first_key.cc
+++ epi_judge_cpp/search_first_key.cc
@@ -3,9 +3,35 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+int SearchFirstOfK1(const vector<int>& A, int k) {
+  // TODO - you fill in here.
+  if (A.empty()) {
+    return -1;
+  }
+  auto target = std::lower_bound(begin(A), end(A), k);
+  int num = *target;
+  if (num == k) {
+    return target - begin(A);
+  }
+  return -1;
+}
 int SearchFirstOfK(const vector<int>& A, int k) {
   // TODO - you fill in here.
-  return 0;
+  int left = 0;
+  int right = A.size() - 1;
+  int resultIndex = -1;
+  while(left <= right) {
+    int mid = left + ((right - left) / 2);
+    if (A[mid] < k) {
+      left = mid + 1;
+    } else if (A[mid] == k) {
+      resultIndex = mid;
+      right = mid - 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return resultIndex;
 }
 
 int main(int argc, char* argv[]) {
