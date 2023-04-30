c3619bd8752b01f37094b88a904498355ffcb842
Ni Qiang <ni.qiang@imt-atlantique.net>
2023-04-30 16:21:00 +0200

    evaluate_rpn

--- epi_judge_cpp/evaluate_rpn.cc
+++ epi_judge_cpp/evaluate_rpn.cc
@@ -1,10 +1,44 @@
 #include <string>
+#include <iostream>
 
 #include "test_framework/generic_test.h"
 using std::string;
 int Evaluate(const string& expression) {
   // TODO - you fill in here.
-  return 0;
+  std::stack<string> expressionStack;
+  int startToFind = 0;
+  while(startToFind < expression.size()) {
+    int commaPos = expression.find(',', startToFind);
+    string expr;
+    if (commaPos == std::string::npos) {
+      expr = expression.substr(startToFind);
+      startToFind = expression.size();
+    } else {
+     expr = expression.substr(startToFind, commaPos - startToFind);
+    startToFind = commaPos + 1;
+    }
+    if (expr == "+" || expr == "-" || expr == "*" || expr == "/") {
+      string operand1 = expressionStack.top();
+      //std::cout << "test " << operand1 << std::endl; 
+      int64_t ope1 = std::stoll(operand1);
+      expressionStack.pop();
+      string operand2 = expressionStack.top();
+      int64_t ope2 = std::stoll(operand2);
+      expressionStack.pop();
+      int64_t result;
+      if (expr == "+") result = ope2 + ope1;
+      if (expr == "-") result = ope2 - ope1;
+      if (expr == "*") result = ope2 * ope1;
+      if (expr == "/") result = ope2 / ope1;
+      expressionStack.push(std::to_string(result));
+      //std::cout << "stack: "<< expressionStack
+      //std::cout << operand1 << " " << expr << " " << operand2 << " = " << result << std::endl;
+    } else {
+      expressionStack.push(expr);
+    }
+  }
+  return std::stoi(expressionStack.top());
+
 }
 
 int main(int argc, char* argv[]) {
