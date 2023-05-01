523076648e8364e8f99a2f94f7f00d42e9b17592
Ni Qiang <ni.qiang@imt-atlantique.net>
2023-05-01 15:58:13 +0200

    intersect

--- epi_judge_cpp/intersect_sorted_arrays.cc
+++ epi_judge_cpp/intersect_sorted_arrays.cc
@@ -6,7 +6,23 @@ using std::vector;
 vector<int> IntersectTwoSortedArrays(const vector<int>& A,
                                      const vector<int>& B) {
   // TODO - you fill in here.
-  return {};
+  int a = 0, b = 0;
+  vector<int> result;
+  while(a < A.size() && b < B.size()) {
+    if (A[a] < B[b]) {
+      a++;
+    } else if (A[a] > B[b]) {
+      b++;
+    } else {
+      if (a == 0 || A[a] != A[a - 1]) {
+        //std::cout << A[a] << std::endl;
+        result.push_back(A[a]);
+        
+      }
+      a++; b++;
+    }
+  }
+  return result;
 }
 
 int main(int argc, char* argv[]) {
