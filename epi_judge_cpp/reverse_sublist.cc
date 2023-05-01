ada4bc5fd327e35b0915e0623f21004b6dd743b6
Ni Qiang <ni.qiang@imt-atlantique.net>
2023-05-01 14:49:38 +0200

    reverse_sublist

--- epi_judge_cpp/reverse_sublist.cc
+++ epi_judge_cpp/reverse_sublist.cc
@@ -3,8 +3,54 @@
 
 shared_ptr<ListNode<int>> ReverseSublist(shared_ptr<ListNode<int>> L, int start,
                                          int finish) {
+  
   // TODO - you fill in here.
-  return nullptr;
+  /*if (!L) {
+    return nullptr;
+  }
+  shared_ptr<ListNode<int>> prev = nullptr;
+  auto cur = L;
+  int i = 1;
+  while(start > 1) {
+    prev = cur;
+    cur = cur->next;
+    start--;
+    finish--;
+  }
+  auto con = prev;
+  auto tail = cur;
+
+  shared_ptr<ListNode<int>> third = nullptr;
+  while(finish > 0) {
+    third = cur->next;
+    cur->next = prev;
+    prev = cur;
+    cur = third;
+    finish--;
+    i++;
+  }
+
+  if (con != nullptr) {
+    con->next = prev;
+  } else {
+    L = prev;
+  }
+  tail->next = cur;
+  return nullptr;*/
+  auto dummy_head = make_shared<ListNode<int>>(ListNode<int>{0, L});
+  auto sublist_head = dummy_head;
+  int k = 1;
+  while(k++ < start) {
+    sublist_head = sublist_head->next;
+  }
+  auto sublist_iter = sublist_head->next;
+  while(start++ < finish) {
+    auto temp = sublist_iter->next;
+    sublist_iter->next = temp->next;
+    temp->next = sublist_head->next;
+    sublist_head ->next = temp;
+  }
+  return dummy_head->next;
 }
 
 int main(int argc, char* argv[]) {
